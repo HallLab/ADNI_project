863fbe3f4266c5a8adae5d2e91467db4998cd902
Tomas Gonzalez Zarzar <tomasgzarzar@gmail.com>
2021-07-15 11:05:22 -0400

    - wgcna analysis on metabolites - wgcna sex module comparison

--- run_cluster.sh
+++ run_cluster.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -l nodes=1:ppn=1:rhel7
-#PBS -l walltime=1:00:00
+#PBS -l walltime=5:00:00
 #PBS -l pmem=8gb
 #PBS -A mah546_c_g_bc_default #or open
 #PBS -j oe
@@ -15,6 +15,7 @@ conda activate adni_project
 
 # Set up directory
 mkdir results/
+mkdir results/plots
 mkdir data/
 # Copy data sets
 for file in `cat ADNI_data_files.txt`; do cp "$file" "data/"; done
@@ -23,6 +24,8 @@ for file in `cat ADNI_data_files.txt`; do cp "$file" "data/"; done
 cd code/
 echo '-----Running QC-----'
 python 01_QC.py 
+echo '-----Running WGCNA-----'
+Rscript WGCNA.R
 echo '-----Running Analysis-----'
 python 02_PreliminaryAnalysis.py
 
--- run_local.sh
+++ run_local.sh
@@ -2,10 +2,17 @@
 
 #### CREATE DIRECTORIES
 mkdir results/
+mkdir results/plots
 
 #### RUN PIPELINE
 conda activate adni_project
 
+# Run pipeline
 cd code/
+echo '-----Running QC-----'
 python 01_QC.py 
+echo '-----Running WGCNA-----'
+Rscript WGCNA.R
+echo '-----Running Analysis-----'
 python 02_PreliminaryAnalysis.py
+
