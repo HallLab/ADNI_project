5523844873a680ea371bc028b3d3a44abd0c9a3e
Tomas Gonzalez <tomasgzarzar@gmail.com>
2021-09-01 16:25:40 -0400

    nmr and p180 in fig3

--- code/04_GenerateFigures.py
+++ code/04_GenerateFigures.py
@@ -78,34 +78,44 @@ modules_to_highlight = ['green',
                         'turquoise']
 met_to_module_names = ['module_colors_nmr.csv',
                        'module_colors_p180.csv']
-results_metabolites_nmr = ['results_nmr_Component 1.csv',
-                          'results_nmr_Component 2.csv']
-results_metabolites_p180 = ['results_p180_Component 1.csv',
-                            'results_p180_Component 2.csv']
+results_nmr = pd.read_csv(respath + 'results_nmr.csv').\
+                 set_index(['Variable',
+                            'Outcome'])
+results_p180 = pd.read_csv(respath + 'results_p180.csv').\
+                 set_index(['Variable',
+                            'Outcome'])
+components = ['Component 1',
+              'Component 2']
 axis_names = ['Effect in females',
               'Effect in males']
-ax_titles = ['Differences in Component 1',
-             'Differences in Component 2']
+ax_titles = ['Differences in Component 1 (NMR)',
+             'Differences in Component 2 (NMR)',
+             'Differences in Component 1 (p180)',
+             'Differences in Component 2 (p180)']
 
-fig = plt.figure(figsize = (12,6))
-fig.suptitle('Effect size differences NMR', fontsize=16)
-ax1 = fig.add_subplot(1,2,1)
-ax2 = fig.add_subplot(1,2,2)
+fig = plt.figure(figsize = (10,10))
+fig.suptitle('Effect size differences', fontsize=16)
+ax1 = fig.add_subplot(2,2,1)
+ax2 = fig.add_subplot(2,2,2)
+ax3 = fig.add_subplot(2,2,3)
+ax4 = fig.add_subplot(2,2,4)
 axes = [ax1,
-        ax2]
-for i, name in enumerate(results_metabolites_nmr):
-    filename = respath + \
-               name
-    res = pd.read_csv(filename).\
-             set_index(['Variable',
-                        'Outcome'])
-    mod_name = respath + \
-               met_to_module_names[0]
-    mods = pd.read_csv(mod_name, 
-                       header=None)
+        ax2,
+        ax3,
+        ax4]
+for i in range(4):
+    if i < 2:
+        mod_name = respath + \
+                   met_to_module_names[0]
+        mods = pd.read_csv(mod_name, 
+                           header=None)
+        res = results_nmr.xs(components[i],
+                             level="Outcome")
+    else:
+        res = results_p180.xs(components[i-2],
+                              level="Outcome")
     max_value = max(abs(res.loc[:,['Beta_female',
                                    'Beta_male'] ]).max())
-    pvals = -np.log(res['pvalue_diff'])
     max_value_round = round(max_value + 0.1, 2)
     min_max   = (-max_value_round,
                  max_value_round)
@@ -115,24 +125,35 @@ for i, name in enumerate(results_metabolites_nmr):
                     color = 'grey',
                     alpha = 0.4,
                     s = pvals*20)
-    for color in modules_to_highlight:
-        module_bool = mods[1] == color
-        module_metabolites = mods[0][module_bool]
-        axes[i].scatter(res.loc[module_metabolites,'Beta_female'],
-                        res.loc[module_metabolites,'Beta_male'],
-                        color = color,
-                        s = pvals[module_metabolites]*20,
-                        alpha = 0.8)
-    axes[i].set_xlabel(axis_names[0], fontsize = 14)
-    axes[i].set_ylabel(axis_names[1], fontsize = 14)
-    axes[i].set_title(ax_titles[i], fontsize = 15)
+    if i < 2:
+        for color in modules_to_highlight:
+            module_bool = mods[1] == color
+            module_metabolites = mods[0][module_bool]
+            axes[i].scatter(res.loc[module_metabolites,'Beta_female'],
+                            res.loc[module_metabolites,'Beta_male'],
+                            color = color,
+                            s = pvals[module_metabolites]*20,
+                            alpha = 0.7)
+    else:
+        class_to_highlight = ['Sex-specific',
+                              'Heterogeneous']
+        for cat in class_to_highlight:
+            class_bool = res['difference_arnold'] == cat
+            axes[i].scatter(res.loc[class_bool,'Beta_female'],
+                            res.loc[class_bool,'Beta_male'],
+                            color = 'red',
+                            s = pvals[class_bool]*20,
+                            alpha = 0.7)
+    axes[i].set_xlabel(axis_names[0], fontsize = 12)
+    axes[i].set_ylabel(axis_names[1], fontsize = 12)
+    axes[i].set_title(ax_titles[i], fontsize = 14)
     axes[i].set_xlim(min_max)
     axes[i].set_ylim(min_max)
     axes[i].axhline(y=0, color='k')
     axes[i].axvline(x=0, color='k')
 
 filename = savepath +\
-           'Figure2.pdf'
+           'Figure3.pdf'
 plt.savefig(filename,
             dpi=300)
 
